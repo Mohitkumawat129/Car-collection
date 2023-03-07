6346dcf7f537c980adb0dfaff2397236ad49c9ab
Mohit kumawat <mohitkumawat129@gmail.com>
2023-03-07 12:36:18 +0530

    Initial commit

---
+++ src/CarData.js
@@ -0,0 +1,56 @@
+const CarData = [
+  {
+    imgSrc:
+      "https://imgd-ct.aeplcdn.com/664x415/n/cw/ec/44709/fortuner-exterior-left-front-three-quarter.jpeg?q=75",
+    title: "TOYOTA",
+    CName: "FORTUNER",
+    link: "https://www.toyotabharat.com/showroom/fortuner/index-fortuner.html",
+  },
+  {
+    imgSrc: "https://imgd.aeplcdn.com/1056x594/n/19qeasa_1459071.jpg?q=75",
+
+    title: "FORD",
+    CName: "ENDEAVOUR",
+    link: "https://www.india.ford.com/new-test-drive-endeavour/",
+  },
+
+  {
+    imgSrc:
+      "https://images.hindustantimes.com/auto/img/2022/08/31/1600x900/MG_Gloster_1661933715189_1661933723744_1661933723744.jpeg",
+
+    title: "MG",
+    CName: "GLOSTER",
+    link: "https://www.mgmotor.co.in/vehicles/mggloster",
+  },
+  {
+    imgSrc:
+      "https://imgeng.jagran.com/images/2022/dec/Mahindra%20Alturas1670228903774.jpg",
+
+    title: "MAHINDRA",
+    CName: "ALTURAS",
+    link: "https://auto.mahindra.com/suv/alturasg4",
+  },
+  {
+    imgSrc:
+      "https://gaadiwaadi.com/wp-content/uploads/2020/10/2021-Isuzu-MU-X-Front-6.jpg",
+
+    title: "ISUZU",
+    CName: "MUX",
+    link: "https://www.isuzu.in/product/isuzu-mu-x/",
+  },
+  {
+    imgSrc:
+      "https://cdni.autocarindia.com/Utils/ImageResizer.ashx?n=https://cdni.autocarindia.com/Galleries/20210201095422_Tata_Safari_static.jpg&w=730&h=484&q=75&c=1",
+    title: "TATA",
+    CName: "SAFARI",
+    link: "https://cars.tatamotors.com/suv/safari",
+  },
+  // {
+  //   imgSrc:
+
+  //   title: ,
+  //   CName: ,
+  //   link: ,
+  // },
+];
+export default CarData;
